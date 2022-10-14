0ad2f762c3a43b5b3e8ad72f83d8091c8b73640e
RaizaCirne <raizabraz93@gmail.com>
2022-10-13 22:54:57 -0300

    docs: inserir exemplo operadores compostos

---
+++ 1º Semestre/Missão Conceitual 3 - Python Básico/Módulo 2 - Variáveis em Python/exemplo-operadores-compostos01.py
@@ -0,0 +1,6 @@
+x = 10             # x recebe o valor 10
+print(f'x={x}')    # Imprimir o valor de x, que é o 10
+x+=2               # Incrementar o X de 2, ou seja, somar 2 ao valor de X usando o operador composto +=
+print(f'x={x}')    # Imprimir valor de x que agora é 12
+x-=2               # Tirar 2 de X, ou seja, subtrair
+print(f"x={x}")    # Ao imprimir temos X = 10
---
+++ 1º Semestre/Missão Conceitual 3 - Python Básico/Módulo 2 - Variáveis em Python/exemplo-operadores-compostos02.py
@@ -0,0 +1,7 @@
+x = 10             # x recebe 10
+x*=2               # Multiplicar o valor de x por 2
+print(f"x={x}")    # O resultado na tela é 20
+x/=2               # Divido o valor de x por 2
+print(f"x={x}")    # 20/ 2 dá 10
+x%=2               # Calcular o resto da divisão inteira de x por 2
+print(f"x{x}")     # 10/ 2 = 5 e o resto da divisão é zero. A tela mostra 0
