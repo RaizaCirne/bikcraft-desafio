be46e1773d7d110841965908db0887fa7c91c7c8
RaizaCirne <raizabraz93@gmail.com>
2022-10-14 18:36:53 -0300

    docs: inserir exemplo lista sequencial

---
+++ 1º Semestre/Missão Conceitual 3 - Python Básico/Módulo 2 - Variáveis em Python/exemplo-lista-sequencial.py
@@ -0,0 +1,11 @@
+lista = [101,202,303,404,505]   # Escolhi o nome lista e faço uma atribuição com = Tenho 5 elementos nessa lista
+print(f'lista[0] = {lista[0]}') # Lista zero acessa o elemento 101
+print(f'lista[1] = {lista[1]}') # Lista 1 acessa o elemento 202
+print(f'lista[2] = {lista[2]}') # Lista 2 acessa o elemento 303
+print(f'lista[3] = {lista[3]}') # Lista 3 acessa o elemento 404
+print(f'lista[4] = {lista[4]}') # Lista 4 acessa o elemento 505
+print(f'lista completa: {lista}') # Imprime a lista completa na tela
+
+# Python trabalha com indexação.
+# No Python a indexação começa na posição zero e pra acessar um valor temos que falar a posição que ele está ocupando dentro da lista
+# 101 está na lista zero 0
