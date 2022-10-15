3f6fb43b6ae0ea1bd55e6b043cdab5b2a10c3875
RaizaCirne <raizabraz93@gmail.com>
2022-10-15 14:12:51 -0300

    docs: inserir exercicio 03

---
+++ 1º Semestre/Missão Conceitual 3 - Python Básico/Módulo 4 - Atribuição, entrada e saída de dados em Python/Exercícios/exercicio03.py
@@ -0,0 +1,9 @@
+# Obter a raíz da equação do primeiro grau
+
+#f(x) = ax+b
+# x = -b/a com a diferente de zero
+
+a = 10
+b = 5
+x = -b/a
+print('A raiz da equação do primeiro grau é: x{}'.format(x))
