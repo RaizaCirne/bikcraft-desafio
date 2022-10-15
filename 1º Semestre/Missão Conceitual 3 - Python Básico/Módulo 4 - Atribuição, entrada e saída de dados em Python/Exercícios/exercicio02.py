a1cb7f08511f48b924dd3913c1d63ec2c81c79f0
RaizaCirne <raizabraz93@gmail.com>
2022-10-15 14:05:58 -0300

    docs: inserir exercicio 02

---
+++ 1º Semestre/Missão Conceitual 3 - Python Básico/Módulo 4 - Atribuição, entrada e saída de dados em Python/Exercícios/exercicio02.py
@@ -0,0 +1,14 @@
+# Exercício 02
+# Considere as listas abaixo
+
+a = ['10']
+b = ['20']
+c = ['30']
+
+# Qual o resultado da operação a *2 + b * 3 + c*4
+# a*2 - Repetiu o 10 duas vezes
+# b*3 - Repetiu o 20 três vezes
+# c*4 - Repetiu o 30 quatro vezes
+
+r = a *2 + b * 3 + c*4   # Essa soma, quando trabalhamos com listas ela transforma com contatenação
+print(f'r={r}')
