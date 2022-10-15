6cd377205241ca6edd295dd22f9350e0d353ba08
RaizaCirne <raizabraz93@gmail.com>
2022-10-15 13:57:19 -0300

    docs: inserir exercicio 01

---
+++ 1º Semestre/Missão Conceitual 3 - Python Básico/Módulo 4 - Atribuição, entrada e saída de dados em Python/Exercícios/exercicio01.py
@@ -0,0 +1,13 @@
+# Considere as listas abaixo
+
+a = ['10']
+b = ['20']
+c = ['30']
+
+# Qual o resultado da operação a + b + c?
+
+r = a + b + c       # Guardei a soma na variável r
+print(f'r={r}')     # Para imprimir o resultado
+
+# Resultado na tela -> r=['10', '20', '30']
+# Tivemos uma contatenação. As 3 listas se transformaram em uma única lista com os valores
