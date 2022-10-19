23b779d6d779fecd3f443b9b47fd7fd7d8755adb
RaizaCirne <raizabraz93@gmail.com>
2022-10-19 17:47:54 -0300

    docs: inserir exemplo funcoes

--- 1º Semestre/Nível 2/Missão Conceitual 4 - Python Estruturado/Módulo 1 - Estruturas de repetição em Python/python-estruturado-material/codigo2.py
+++ 1º Semestre/Nível 2/Missão Conceitual 4 - Python Estruturado/Módulo 1 - Estruturas de repetição em Python/python-estruturado-material/codigo2.py
@@ -1,2 +1,2 @@
-for item in range(2, 9, 3):
-    print(item)
+for item in range(2, 9, 3): # A linha 1 mostra a criação do laço com a variável item percorrendo a sequência (2, 5, 8), que é criada pela chamada range(2, 9, 3);
+    print(item)            # A linha 2 indica a instrução que será executada para cada repetição desse laço. O laço for executa a instrução da linha 2 três vezes, uma para cada elemento da sequência (2, 5, 8).
--- 1º Semestre/Nível 2/Missão Conceitual 4 - Python Estruturado/Módulo 1 - Estruturas de repetição em Python/python-estruturado-material/codigo5.py
+++ 1º Semestre/Nível 2/Missão Conceitual 4 - Python Estruturado/Módulo 1 - Estruturas de repetição em Python/python-estruturado-material/codigo5.py
@@ -1,4 +1,6 @@
-﻿palavra = input('Entre com uma palavra: \n')
-while palavra != 'sair':
-    palavra = input('Digite sair para encerrar o laço: \n')
-print('Você digitou sair e agora está fora do laço')
+﻿palavra = input('Entre com uma palavra: \n')                  # A linha 1 representa a solicitação ao usuário para que ele insira uma palavra a ser armazenada na variável palavra;
+while palavra != 'sair':                                      # A linha 2 cria o laço while, que depende da condição <valor da variável palavra ser diferente de ‘sair’>;
+    palavra = input('Digite sair para encerrar o laço: \n')   # A linha 3 será repetida enquanto a condição for verdadeira, ou seja, enquanto o valor da variável palavra for diferente de ‘sair’. Quando esses valores forem iguais, a condição do laço while será falsa e o laço, encerrado;
+print('Você digitou sair e agora está fora do laço')          # A linha 4 representa a impressão da mensagem fora do laço while.
+
+#obs: Perceba que, ao digitar ‘sair’ logo na primeira solicitação, a linha 3 do nosso programa não é executada nenhuma vez. Ou seja, o programa nem chega a entrar no laço while.
